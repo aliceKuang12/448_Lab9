9179ce3de990d922baee78181098a255f833db35
Alice <66434350+aliceKuang12@users.noreply.github.com>
2022-04-22 23:48:33 -0500

    Add bool variable, preventingDefault reaction

--- formChecker.js
+++ formChecker.js
@@ -1,29 +1,38 @@
-
 function validate() {
+
+	//validate user and password not null
+	var v1 = document.getElementById("user").value;
+	var v2 = document.getElementById("pass").value;
+	var invalid = false;
 	
-  //validate user and password not null
-  var v1 = document.getElementById("user").value;
-  var v2 = document.getElementById("pass").value;
+	if(v1 == ""|| v2 == "") {
+			alert("Login fields must have least one character.");
+			invalid = true;
+	}
 
-  if(v1 == "") {
-	  alert("Username must have least one character.");
-  }
-  if(v2=="")  {
-      alert("Password must have least one character.");
-  }
+	//validate number entries
+	var n1 = document.getElementById("pizzaNum").value;
+	var n2 = document.getElementById("dpgNum").value;
+	var n3 = document.getElementById("chocNum").value;
+	if(n1 == ""||n2==""||n3=="") {
+		alert("Cannot have empty field!");
+		invalid = true;
+	}
+	
+	if(n1 < 0 || n2 < 0 ||n3 < 0) {
+			alert("Negative values not allowed! Indicate 0 or more items.");
+			invalid = true;
+	}
 
-  //validate number entries
-  var n1 = document.getElementById("pizzaNum").value;
-  var n2 = document.getElementById("dpgNum").value;
-  var n3 = document.getElementById("chocNum").value;
+	//validate radio button selected
+	var checked = document.querySelector("input[type=radio]:checked");
+	if (!checked) {
+		alert("You need to select a shipping option");
+		invalid = true;
+	}
+	if(invalid)
+	{
+		event.preventDefault();
+	}
 
-  if(n1 < 0 || n2 < 0 ||n3 < 0) {
-          alert("Cannot have a negative value!");
-  }
-  
-//validate radio button selected
-var checked = document.querySelector("input[type=radio]:checked");
- if (!checked) {
-        alert("You need to select a shipping option");
- }
 }
